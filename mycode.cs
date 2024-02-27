dd6b7e9a0e567b34b5d3a491470e70df0190a103
Daisy Rene <161464593+sairenedais@users.noreply.github.com>
2024-02-27 14:04:38 +0200

    Create mycode.cs

---
+++ mycode.cs
@@ -0,0 +1,27 @@
+  public void AddService(Type type, object provider)
+        {
+            if (type == null)
+                throw new ArgumentNullException("type");
+            if (provider == null)
+                throw new ArgumentNullException("provider");
+            if (!ReflectionHelpers.IsAssignableFrom(type, provider))
+                throw new ArgumentException("The provider does not match the specified service type!");
+
+            services.Add(type, provider);
+        }
+
+
+       public object GetService(Type type)
+        {
+            if (type == null)
+                throw new ArgumentNullException("type");
+						
+            object service;
+            if (services.TryGetValue(type, out service))
+                return service;
+
+            return null;
+        }    public static float ClassifyPoint(ref Vector3 point, ref Plane plane)
+        {
+            return point.X * plane.Normal.X + point.Y * plane.Normal.Y + point.Z * plane.Normal.Z + plane.D;
+      
